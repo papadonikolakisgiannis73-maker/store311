bac7a529db59d440655807bd711539bbf14eca2f
papadonikolakisgiannis73-maker <papadonikolakisgiannis73@gmail.com>
2025-10-20 21:37:59 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'store311-v2';
+const CACHE = 'store311-v3';
 const ASSETS = ['./','./index.html','./manifest.json','./logo.png'];
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
